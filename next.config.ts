efc777e671fb310eddf751eafe98e7b339981ce0
oodoabel <get2abeloodo@gmail.com>
2025-07-26 13:40:23 +0100

    alternative access

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,8 @@ module.exports = {
         "fjkks9rr-3000.uks1.devtunnels.ms",
         "nomination-site.vercel.app",
         "https://nomination-site.vercel.app",
+        "https://nomination-site-4eut5hjjv-abels-projects-46ed9b6e.vercel.app",
+        "nomination-site-4eut5hjjv-abels-projects-46ed9b6e.vercel.app",
       ],
     },
   },
