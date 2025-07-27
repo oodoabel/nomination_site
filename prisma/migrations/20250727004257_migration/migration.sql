2c4d13cfabc888617506d0c0fbc80eef4bc1c66f
oodoabel <get2abeloodo@gmail.com>
2025-07-27 02:51:20 +0100

    Remove unused database files and migration scripts

--- prisma/migrations/20250727004257_migration/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Nomination" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "nominee" TEXT NOT NULL,
-    "category" TEXT NOT NULL,
-    "quantity" INTEGER NOT NULL
-);
